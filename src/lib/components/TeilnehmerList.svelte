cad5ef9ab60e6ec696f7f5a424716f9186d84d61
shajiranraveendran <raveesha@students.zhaw.ch>
2024-12-24 10:17:35 +0100

    Components 3/5 - TeilnehmerList (OK)

---
+++ src/lib/components/TeilnehmerList.svelte
@@ -0,0 +1,22 @@
+<script>
+    let { data } = $props();
+</script>
+
+<table>
+    <thead>
+        <tr>
+            <th scope="col">Vorname</th>
+            <th scope="col">Nachname</th>
+            <th scope="col">Email</th>
+        </tr>
+    </thead>
+    <tbody>
+        {#each data.teilnehmer as person}
+            <tr>
+                <td>{person.vorname}</td>
+                <td>{person.nachname}</td>
+                <td>{person.email}</td>
+            </tr>
+        {/each}
+    </tbody>
+</table>
--- src/routes/teilnehmer/+page.svelte
+++ src/routes/teilnehmer/+page.svelte
@@ -1,5 +1,6 @@
 <script>
     import TeilnehmerForm from "$lib/components/TeilnehmerForm.svelte";
+    import TeilnehmerList from "$lib/components/TeilnehmerList.svelte";
 
     let teilnehmer = {
         vorname: "",
@@ -13,23 +14,4 @@
 <h1>TEILNEHMER</h1>
 
 <TeilnehmerForm {teilnehmer}></TeilnehmerForm>
-
-<h3>Liste von allen Teilnehmer</h3>
-<table>
-    <thead>
-        <tr>
-            <th>Vorname</th>
-            <th>Nachname</th>
-            <th>Email</th>
-        </tr>
-    </thead>
-    <tbody>
-        {#each data.teilnehmer as person}
-            <tr>
-                <td>{person.vorname}</td>
-                <td>{person.nachname}</td>
-                <td>{person.email}</td>
-            </tr>
-        {/each}
-    </tbody>
-</table>
+<TeilnehmerList {data}></TeilnehmerList>
