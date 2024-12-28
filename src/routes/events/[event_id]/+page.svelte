03d1acbb052f5fb9d577d7dde95f2d4185aee8b1
shajiranraveendran <raveesha@students.zhaw.ch>
2024-12-28 12:23:48 +0100

    VERBINDUNG: Teilnehmer (ID) werden als Array gespeichert

--- src/routes/events/[event_id]/+page.svelte
+++ src/routes/events/[event_id]/+page.svelte
@@ -1,23 +1,32 @@
-<!-- SCRIPT -->
 <script>
     import EventEdit from "$lib/components/EventEdit.svelte";
+    import TeilnehmerList from "$lib/components/TeilnehmerList.svelte";
 
     let { data, form } = $props();
 </script>
 
-
-<!-- HTML -->
 <h1>EVENT DETAIL</h1>
 
 <!-- BACK BUTTON -->
 <a href="/events" class="btn" role="button">BACK</a><br /><br />
 
-<!-- MESSAGE -->
+<!-- Erfolgsnachricht für Event-Update -->
 {#if form?.success}
     <div class="alert alert-primary mt-3">
-        EVENT: "{form.eventname}" wurde aktualisiert!
+        Event: "{form?.eventname}" wurde erfolgreich aktualisiert!
     </div>
 {/if}
 
-<!-- EVENT EDIT-->
-<EventEdit {data} {form} />
+<!-- EVENT EDIT -->
+<EventEdit {data} {form} /><br />
+
+<!-- Teilnehmer Hinzufügen -->
+<form method="POST" action="?/addTeilnehmer">
+    <label for="teilnehmer">CHOOSE TEILNEHMER</label><br />
+    <select name="teilnehmerId" id="teilnehmer" class="form-control">
+        {#each data.teilnehmer as teilnehmer}
+            <option value={teilnehmer._id}>{teilnehmer.vorname} {teilnehmer.nachname}</option>
+        {/each}
+    </select><br />
+    <button class="btn btn-primary mt-2">ADD TEILNEHMER</button>
+</form>
